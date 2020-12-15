6a6164128e5b1f73e07b5eff46d6622f26d046db
tommycwu <tommycwu@users.noreply.github.com>
2020-12-14 22:38:47 -0800

    inital commit

---
+++ Index.aspx
@@ -0,0 +1,175 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="PocOrgSetup.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 130px;
+        }
+        .auto-style7 {
+            width: 372px;
+        }
+        .auto-style9 {
+            width: 123px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">
+                    &nbsp;</td>
+                <td class="auto-style9">
+        <asp:Label ID="Label1" runat="server" Font-Names="Arial" Font-Size="Small" Text="Org:"></asp:Label>
+                </td>
+                <td class="auto-style7"><asp:TextBox ID="TextBox1" runat="server" Width="360px">https://nextpoc.okta.com</asp:TextBox>
+                </td>
+                <td class="auto-style9"><asp:Label ID="Label2" runat="server" Font-Names="Arial" Font-Size="Small" Text="Key:"></asp:Label>
+                </td>
+                <td><asp:TextBox ID="TextBox2" runat="server" Width="360px">00_66otAllJp2xgJB4zryTt76xeQcHN3syuCRztDhE</asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="GET - PolicyId, GroupId" Height="24px" Width="180px" />
+                </td>
+                <td class="auto-style9">
+        <asp:Label ID="Label4" runat="server" Font-Names="Arial" Font-Size="Small" Text="PolicyId:"></asp:Label>
+                </td>
+                <td class="auto-style7"><asp:TextBox ID="TextBoxPolicyId" runat="server" Width="360px"></asp:TextBox>
+                </td>
+                <td class="auto-style9">
+        <asp:Label ID="Label7" runat="server" Font-Names="Arial" Font-Size="Small" Text="[Everybody] GroupId:"></asp:Label>
+                </td>
+                <td><asp:TextBox ID="TextBoxGroupId" runat="server" Width="360px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td class="auto-style9">&nbsp;</td>
+                <td class="auto-style7">&nbsp;</td>
+                <td class="auto-style9">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="POST - Enable Threat" Height="24px" Width="180px" />
+                </td>
+                <td class="auto-style9">&nbsp;</td>
+                <td class="auto-style7">
+        <asp:TextBox ID="TextBoxPostThreatData" runat="server" Height="180px" Width="360px" TextMode="MultiLine">{&quot;action&quot;: &quot;block&quot;,&quot;excludeZones&quot;: []}</asp:TextBox>
+                </td>
+                <td class="auto-style9">&nbsp;</td>
+                <td>
+        <asp:TextBox ID="TextBoxPostThreatReturn" runat="server" Height="180px" TextMode="MultiLine" Width="360px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td class="auto-style9">&nbsp;</td>
+                <td class="auto-style7">
+                    &nbsp;</td>
+                <td class="auto-style9">
+                    &nbsp;</td>
+                <td>
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td class="auto-style9">&nbsp;</td>
+                <td class="auto-style7">&nbsp;</td>
+                <td class="auto-style9">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    &nbsp;</td>
+                <td class="auto-style9">
+                    &nbsp;</td>
+                <td class="auto-style7">&nbsp;</td>
+                <td class="auto-style9">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    &nbsp;</td>
+                <td class="auto-style9">
+                    &nbsp;</td>
+                <td class="auto-style7">&nbsp;</td>
+                <td class="auto-style9">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="POST - Enable Push" Height="24px" Width="180px" />
+                </td>
+                <td class="auto-style9">&nbsp;</td>
+                <td class="auto-style7">
+        <asp:TextBox ID="TextBoxPostPushData" runat="server" Height="180px" Width="360px" TextMode="MultiLine">{&quot;type&quot;: &quot;MFA_ENROLL&quot;,&quot;status&quot;: &quot;ACTIVE&quot;,&quot;name&quot;: &quot;MFA Enroll&quot;,&quot;priority&quot;: 1,&quot;system&quot;: false,&quot;conditions&quot;: {&quot;people&quot;: {&quot;users&quot;: {&quot;exclude&quot;: []}},&quot;network&quot;: {&quot;connection&quot;: &quot;ANYWHERE&quot;}},&quot;actions&quot;: {&quot;enroll&quot;: {&quot;self&quot;: &quot;LOGIN&quot;}}}</asp:TextBox>
+                </td>
+                <td class="auto-style9">&nbsp;</td>
+                <td>
+        <asp:TextBox ID="TextBoxPostPushReturn" runat="server" Height="180px" TextMode="MultiLine" Width="360px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td class="auto-style9">&nbsp;</td>
+                <td class="auto-style7">
+                    &nbsp;</td>
+                <td class="auto-style9">
+                    &nbsp;</td>
+                <td>
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td class="auto-style9">&nbsp;</td>
+                <td class="auto-style7">
+                    &nbsp;</td>
+                <td class="auto-style9">
+                    &nbsp;</td>
+                <td>
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td class="auto-style9">&nbsp;</td>
+                <td class="auto-style7">
+                    &nbsp;</td>
+                <td class="auto-style9">
+                    &nbsp;</td>
+                <td>
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td class="auto-style9">&nbsp;</td>
+                <td class="auto-style7">
+                    &nbsp;</td>
+                <td class="auto-style9">
+                    &nbsp;</td>
+                <td>
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td class="auto-style9">&nbsp;</td>
+                <td class="auto-style7">&nbsp;</td>
+                <td class="auto-style9">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+        <br />
+    </form>
+</body>
+</html>
---
+++ Index.aspx.cs
@@ -0,0 +1,435 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using Okta.Sdk;
+using Okta.Sdk.Configuration;
+
+namespace PocOrgSetup
+{    public partial class Index : System.Web.UI.Page
+    {
+
+        public string GetMfaPolicyId(string baseUrl, string apiKey)
+        {
+            var userUrl = baseUrl + "/api/v1/policies?type=MFA_ENROLL";
+            List<string> responseList = new List<string>(new string[] { "" });
+            try
+            {
+                var endpoint = new Uri(userUrl);
+                var webRequest = WebRequest.Create(endpoint) as HttpWebRequest;
+                if (webRequest != null)
+                {
+                    webRequest.Method = "GET";
+                    webRequest.Headers.Add("Authorization", "SSWS " + apiKey);
+                    webRequest.Accept = "application/json";
+                    webRequest.ContentType = "application/json";
+                    var response = webRequest.GetResponse();
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string jsonString = reader.ReadToEnd();
+                        dynamic jsonObject = JsonConvert.DeserializeObject(jsonString, typeof(object));
+                        for (int i = 0; i <= jsonObject.Count - 1; i++)
+                        {
+                            var s = jsonObject[i].ToString();
+                            if (s.Contains("id"))
+                            {
+                                return jsonObject[i].id;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return "";
+        }
+
+        public string GetEverybodyGroup(string baseUrl, string apiKey)
+        {
+            var userUrl = baseUrl + "/api/v1/groups";
+            List<string> responseList = new List<string>(new string[] { "" });
+            try
+            {
+                var endpoint = new Uri(userUrl);
+                var webRequest = WebRequest.Create(endpoint) as HttpWebRequest;
+                if (webRequest != null)
+                {
+                    webRequest.Method = "GET";
+                    webRequest.Headers.Add("Authorization", "SSWS " + apiKey);
+                    webRequest.Accept = "application/json";
+                    webRequest.ContentType = "application/json";
+                    var response = webRequest.GetResponse();
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string jsonString = reader.ReadToEnd();
+                        dynamic jsonObject = JsonConvert.DeserializeObject(jsonString, typeof(object));
+                        for (int i = 0; i <= jsonObject.Count - 1; i++)
+                        {
+                            var s = jsonObject[i].ToString();
+                            if (s.Contains("id"))
+                            {
+                                return jsonObject[i].id;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return "";
+        }
+        public List<string> GetMfaPolicy(string baseUrl, string apiKey)
+        {
+            var userUrl = baseUrl + "/api/v1/policies?type=MFA_ENROLL";
+            List<string> responseList = new List<string>(new string[] { "" });
+            try
+            {
+                var endpoint = new Uri(userUrl);
+                var webRequest = WebRequest.Create(endpoint) as HttpWebRequest;
+                if (webRequest != null)
+                {
+                    webRequest.Method = "GET";
+                    webRequest.Headers.Add("Authorization", "SSWS " + apiKey);
+                    webRequest.Accept = "application/json";
+                    webRequest.ContentType = "application/json";
+                    var response = webRequest.GetResponse();
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string jsonString = reader.ReadToEnd();
+                        dynamic jsonObject = JsonConvert.DeserializeObject(jsonString, typeof(object));
+                        string s1, s2;
+                        for (int i = 0; i <= jsonObject.Count - 1; i++)
+                        {
+                            s1 = jsonObject[i].settings.factors.ToString();
+                            if (s1.Contains("okta_push"))
+                            {
+                                s2 = jsonObject[i].settings.factors.okta_push.enroll.ToString();
+                                if (s2.Contains("OPTIONAL"))
+                                {
+                                    responseList.Add("okta_push enabled");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                responseList.Add(ex.Message);
+                return responseList;
+            }
+            return responseList;
+        }
+
+        public List<string> GetPasswordPolicies(string baseUrl, string apiKey)
+        {
+            var userUrl = baseUrl + "/api/v1/policies?type=PASSWORD";
+            List<string> responseList = new List<string>(new string[] { "" });
+            try
+            {
+                var endpoint = new Uri(userUrl);
+                var webRequest = WebRequest.Create(endpoint) as HttpWebRequest;
+                if (webRequest != null)
+                {
+                    webRequest.Method = "GET";
+                    webRequest.Headers.Add("Authorization", "SSWS " + apiKey);
+                    webRequest.Accept = "application/json";
+                    webRequest.ContentType = "application/json";
+                    var response = webRequest.GetResponse();
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string jsonString = reader.ReadToEnd();
+                        dynamic jsonObject = JsonConvert.DeserializeObject(jsonString, typeof(object));
+                        string s1;
+                        for (int i = 0; i <= jsonObject.Count - 1; i++)
+                        {
+                            s1 = jsonObject[i].settings.password.complexity.minLength.ToString();
+                            responseList.Add("Pwd Min Len. = " + s1);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                responseList.Add(ex.Message);
+                return responseList;
+            }
+            return responseList;
+        }
+
+        public List<string> GetSignonPolicies(string baseUrl, string apiKey)
+        {
+            var userUrl = baseUrl + "/api/v1/policies?type=OKTA_SIGN_ON";
+            List<string> responseList = new List<string>(new string[] { "" });
+            try
+            {
+                var endpoint = new Uri(userUrl);
+                var webRequest = WebRequest.Create(endpoint) as HttpWebRequest;
+                if (webRequest != null)
+                {
+                    webRequest.Method = "GET";
+                    webRequest.Headers.Add("Authorization", "SSWS " + apiKey);
+                    webRequest.Accept = "application/json";
+                    webRequest.ContentType = "application/json";
+                    var response = webRequest.GetResponse();
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string jsonString = reader.ReadToEnd();
+                        dynamic jsonObject = JsonConvert.DeserializeObject(jsonString, typeof(object));
+                        string s1;
+                        for (int i = 0; i <= jsonObject.Count - 1; i++)
+                        {
+                            s1 = jsonObject[i].name;
+                            responseList.Add(s1);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                responseList.Add(ex.Message);
+                return responseList;
+            }
+            return responseList;
+        }
+
+        public List<string> GetAllUsers(string baseUrl, string apiKey)
+        {
+            var userUrl = baseUrl + "/api/v1/users/";
+            List<string> responseList = new List<string>(new string[] { "" });
+            try
+            {
+                var endpoint = new Uri(userUrl);
+                var webRequest = WebRequest.Create(endpoint) as HttpWebRequest;
+                if (webRequest != null)
+                {
+                    webRequest.Method = "GET";
+                    webRequest.Headers.Add("Authorization", "SSWS " + apiKey);
+                    webRequest.Accept = "application/json";
+                    webRequest.ContentType = "application/json";
+                    var response = webRequest.GetResponse();
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string jsonString = reader.ReadToEnd();
+                        dynamic jsonObject = JsonConvert.DeserializeObject(jsonString, typeof(object));
+                        string s1, s2;
+                        for (int i = 0; i <= jsonObject.Count - 1; i++)
+                        {
+                            s1 = jsonObject[i].profile.login;
+                            s2 = jsonObject[i].id;
+                            responseList.Add(s1 + ", " + s2);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                responseList.Add(ex.Message);
+                return responseList;
+            }
+            return responseList;
+        }
+        public List<string> GetAllApps(string baseUrl, string apiKey)
+        {
+            var appsUrl = baseUrl + "/api/v1/apps/";
+            List<string> responseList = new List<string>(new string[] { "" });
+            try
+            {
+                var endpoint = new Uri(appsUrl);
+                var webRequest = WebRequest.Create(endpoint) as HttpWebRequest;
+                if (webRequest != null)
+                {
+                    webRequest.Method = "GET";
+                    webRequest.Headers.Add("Authorization", "SSWS " + apiKey);
+                    webRequest.Accept = "application/json";
+                    webRequest.ContentType = "application/json";
+                    var response = webRequest.GetResponse();
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string jsonString = reader.ReadToEnd();
+                        dynamic jsonObject = JsonConvert.DeserializeObject(jsonString, typeof(object));
+                        string s1, s2;
+                        for (int i = 0; i <= jsonObject.Count - 1; i++)
+                        {
+                            s1 = jsonObject[i].name;
+                            s2 = jsonObject[i].id;
+                            responseList.Add(s1 + ", " + s2);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                responseList.Add(ex.Message);
+                return responseList;
+            }
+            return responseList;
+        }
+
+        public List<string> GetBetaFeatures(string baseUrl, string apiKey)
+        {
+            List<string> responseList = new List<string>(new string[] { "" });
+            try
+            {
+                var client = new OktaClient(new OktaClientConfiguration
+                {
+                    OktaDomain = baseUrl,
+                    Token = apiKey
+                });
+                var returnList = client.Features.ListFeatures().ToEnumerable();
+
+                foreach (var featureObj in returnList)
+                {
+                    responseList.Add(featureObj.Name);
+                }
+            }
+            catch (Exception ex)
+            {
+                responseList.Add(ex.Message);
+                return responseList;
+            }
+            return responseList;
+        }
+
+        public string PostEnrollRule(string baseUrl, string apiKey, string policyId, string policyData)
+        {
+            var policyUrl = baseUrl + "/api/v1/policies/" + policyId + "/rules";
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var request = new HttpRequestMessage
+                    {
+                        Method = HttpMethod.Post,
+                        RequestUri = new Uri(policyUrl)
+                    };
+                    request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    request.Headers.Authorization = new AuthenticationHeaderValue("SSWS", apiKey);
+                    request.Content = new StringContent(policyData, Encoding.UTF8, "application/json");
+                    var response = client.SendAsync(request);
+                    return response.Result.Content.ReadAsStringAsync().Result;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public string PostThreatConfig(string baseUrl, string apiKey, string configData)
+        {
+            var threatUrl = baseUrl + "/api/v1/threats/configuration";
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var request = new HttpRequestMessage
+                    {
+                        Method = HttpMethod.Post,
+                        RequestUri = new Uri(threatUrl)
+                    };
+                    request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    request.Headers.Authorization = new AuthenticationHeaderValue("SSWS", apiKey);
+                    request.Content = new StringContent(configData, Encoding.UTF8, "application/json");
+                    var response = client.SendAsync(request);
+                    return response.Result.Content.ReadAsStringAsync().Result;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        public string ActivateOktaOtp(string baseUrl, string apiKey)
+        {
+            var policyUrl = baseUrl + "/api/v1/org/factors/okta_otp/lifecycle/activate";
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var request = new HttpRequestMessage
+                    {
+                        Method = HttpMethod.Post,
+                        RequestUri = new Uri(policyUrl)
+                    };
+                    request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    request.Headers.Authorization = new AuthenticationHeaderValue("SSWS", apiKey);
+                    var response = client.SendAsync(request);
+                    return response.Result.Content.ReadAsStringAsync().Result;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var oktaDomain = TextBox1.Text;
+            var apiKey = TextBox2.Text;
+
+            //oktaDomain = "https://nextpoc.okta.com";
+            //apiKey = "00_66otAllJp2xgJB4zryTt76xeQcHN3syuCRztDhE";
+            //oktaDomain = "https://dev-8964037.okta.com";
+            //apiKey = "004PwPLvNfDegg9U0UG9P2GIoBdr5LDTKHbP8BVGpk";
+
+            TextBoxPolicyId.Text = GetMfaPolicyId(oktaDomain, apiKey);
+            TextBoxGroupId.Text = GetEverybodyGroup(oktaDomain, apiKey);
+            var featuresList = GetSignonPolicies(oktaDomain, apiKey);
+            //foreach (var featureObj in featuresList)
+            //{
+            //    TextBoxBetaFeatures.Text += featureObj.ToString() + Environment.NewLine;
+            //}
+            //var mfaList = GetMfaPolicy(oktaDomain, apiKey);
+            //foreach (var mfaObj in mfaList)
+            //{
+            //    TextBoxMfaPolicies.Text += mfaObj.ToString() + Environment.NewLine;
+            //}
+            //var usersList = GetPasswordPolicies(oktaDomain, apiKey);
+            //foreach (var userObj in usersList)
+            //{
+            //    TextBoxPwdPolicies.Text += userObj.ToString() + Environment.NewLine;
+            //}
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            var oktaDomain = TextBox1.Text;
+            var apiKey = TextBox2.Text;
+            var policyData = TextBoxPostPushData.Text;
+            var policyId = TextBoxPolicyId.Text;
+
+            //var postResult = ActivateOktaOtp(oktaDomain, apiKey).ToString();
+            //TextBoxPostReturn.Text = postResult;
+            TextBoxPostPushReturn.Text = PostEnrollRule(oktaDomain, apiKey, policyId, policyData);
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            var oktaDomain = TextBox1.Text;
+            var apiKey = TextBox2.Text;
+            var threatData = TextBoxPostThreatData.Text;
+
+            TextBoxPostThreatReturn.Text = PostThreatConfig(oktaDomain, apiKey, threatData);
+        }
+    }
+}
